d10d4f83d6ba4ced4f4cf444669f3958e9f41272
Hai Anh <haianh6311@gmail.com>
2025-02-25 22:30:15 +0700

    adding .env variable

--- app/database/db.js
+++ app/database/db.js
@@ -1,11 +1,11 @@
 import postgres from "postgres";
 
 const sql = postgres({
-  host: "localhost", // Postgres ip address[s] or domain name[s]
-  port: 5432, // Postgres server port[s]
-  database: "social_media", // Name of database to connect to
-  username: "postgres", // Username of database user
-  password: "haianh6311", // Password of database user
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_DATABASE,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
 });
 
 export default sql;
